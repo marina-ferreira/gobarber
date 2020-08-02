c22a64031aba25a5d83bb226d869b5edc87dc9a6
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-02 18:40:37 -0300

    Forgot password page

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -1,12 +1,12 @@
 import React, { useCallback, useRef } from 'react'
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { FiLogIn, FiMail } from 'react-icons/fi'
 import { Form } from '@unform/web'
 import { Link, useHistory } from 'react-router-dom'
 
 import * as Yup from 'yup'
 
 import getValidationErrors from 'utils/getValidationErrors'
-import { useAuth, useToast } from 'hooks'
+import { useToast } from 'hooks'
 
 import Button from 'components/Button'
 import Input from 'components/Input'
@@ -15,13 +15,11 @@ import logo from 'assets/logo.svg'
 import { Container, Content, Background, AnimationContainer } from './styles'
 
 const schema = Yup.object().shape({
-  email: Yup.string().required().email(),
-  password: Yup.string().required()
+  email: Yup.string().required().email()
 })
 
-const SignIn = () => {
+const ForgotPassword = () => {
   const formRef = useRef(null)
-  const { signIn } = useAuth()
   const { showToast } = useToast()
   const { history } = useHistory()
 
@@ -34,7 +32,6 @@ const SignIn = () => {
           abortEarly: false
         })
 
-        await signIn(data)
         history.push('/dashboard')
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
@@ -45,12 +42,12 @@ const SignIn = () => {
 
         showToast({
           type: 'error',
-          title: 'Authentication Error',
-          description: 'Sign in failed. Invalid credentials.'
+          title: 'Recover Password Error',
+          description: 'An error occured on password recover. Try again.'
         })
       }
     },
-    [signIn, showToast, history]
+    [showToast, history]
   )
 
   return (
@@ -60,7 +57,7 @@ const SignIn = () => {
           <img src={logo} alt="GoBarber" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Sign in</h1>
+            <h1>Recover Password</h1>
 
             <Input
               name="email"
@@ -68,21 +65,13 @@ const SignIn = () => {
               icon={FiMail}
               placeholder="Email"
             />
-            <Input
-              name="password"
-              type="password"
-              icon={FiLock}
-              placeholder="Senha"
-            />
 
-            <Button type="submit">Sign In</Button>
-
-            <Link to="/forgot-password">Forgot my password</Link>
+            <Button type="submit">Recover</Button>
           </Form>
 
-          <Link to="/sign-up">
+          <Link to="/sign-in">
             <FiLogIn />
-            Sign Up
+            Back to sign in
           </Link>
         </AnimationContainer>
       </Content>
@@ -91,4 +80,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default ForgotPassword
