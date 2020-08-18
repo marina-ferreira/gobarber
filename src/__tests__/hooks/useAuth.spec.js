d1a3f9b028bcc93af5054aa0ef758369a6450205
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-17 21:00:29 -0300

    useAuth hook spec | signOut

--- src/__tests__/hooks/useAuth.spec.js
+++ src/__tests__/hooks/useAuth.spec.js
@@ -47,12 +47,13 @@ describe('useAuth Hook', () => {
       user: { email },
       token
     } = apiResponse
-    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+    const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     })
 
-    result.current.signIn({ email, password: '123456' })
-    await waitForNextUpdate()
+    await act(async () => {
+      result.current.signIn({ email, password: '123456' })
+    })
 
     expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:token', token)
     expect(setItemSpy).toHaveBeenCalledWith(
@@ -89,5 +90,37 @@ describe('useAuth Hook', () => {
     })
 
     expect(result.current.user.email).toEqual(user.email)
+  })
+
+  it('should be able to sign out', async () => {
+    const user = {
+      id: 'user123',
+      name: 'John Doe',
+      email: 'johndoe@email.com'
+    }
+
+    jest.spyOn(Storage.prototype, 'setItem').mockImplementation(key => {
+      switch (key) {
+        case '@GoBarber:token':
+          return 'token-123'
+        case '@GoBarber:user':
+          return JSON.stringify(user)
+        default:
+          return null
+      }
+    })
+
+    const removeItemSpy = jest.spyOn(Storage.prototype, 'removeItem')
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    })
+
+    await act(async () => {
+      result.current.signOut()
+    })
+
+    expect(removeItemSpy).toHaveBeenCalledTimes(2)
+    expect(result.current.user).toBeUndefined()
   })
 })
